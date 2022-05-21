81386bfd8b64497c104535d342d9ff1f1ad11281
Asineth <asineth@pm.me>
2022-05-21 14:27:20 -0400

    desktop: improve TS support in preload script

--- packages/client-desktop/src/preload.ts
+++ packages/client-desktop/src/preload.ts
@@ -1,7 +1,30 @@
-const { contextBridge, ipcRenderer } = require("electron");
-const os = require("os");
+import { contextBridge, ipcRenderer } from "electron";
+import os from "os";
 
-let win32Capture;
+interface IWin32CaptureUserOpts {
+  id: string;
+  fps: number;
+  video: boolean;
+  audio: boolean;
+}
+
+interface IWin32CaptureInternalOpts {
+  id: string;
+  fps: number;
+  video: boolean;
+  audio: boolean;
+  buffer: Uint8Array;
+}
+
+let win32Capture:
+  | {
+      startCapture(
+        opts: IWin32CaptureInternalOpts,
+        cb: (data: unknown) => void
+      ): void;
+      stopCapture(): void;
+    }
+  | undefined;
 let win32CaptureBuffer;
 
 const stopWin32Capture = () => {
@@ -10,7 +33,10 @@ const stopWin32Capture = () => {
   }
 };
 
-const startWin32Capture = (opts, cb) => {
+const startWin32Capture = (
+  opts: IWin32CaptureUserOpts,
+  cb: (data: unknown) => void
+) => {
   if (!win32Capture) {
     win32Capture = require("@atriplex-co/hyalus-win32-utils");
   }
@@ -46,7 +72,8 @@ contextBridge.exposeInMainWorld("HyalusDesktop", {
   quit: () => ipcRenderer.invoke("quit"),
   getSources: () => ipcRenderer.invoke("getSources"),
   getStartupSettings: () => ipcRenderer.invoke("getStartupSettings"),
-  setStartupSettings: (val) => ipcRenderer.invoke("setStartupSettings", val),
+  setStartupSettings: (val: unknown) =>
+    ipcRenderer.invoke("setStartupSettings", val),
   osPlatform: os.platform(),
   osRelease: os.release(),
   stopWin32Capture,
